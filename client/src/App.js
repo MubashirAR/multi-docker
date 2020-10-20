0c0b17bc52c71ed4d3c67c2d79eb1d210462e8df
Mubashir Reshamwala <mubresham@gmail.com>
2020-10-20 21:16:31 +0530

    updated header tag

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+          <h1>Fib Calculator</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
